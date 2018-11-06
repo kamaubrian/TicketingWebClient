b4070b96403a8168efa373b1047c93c07396f799
mtotodev05 <mtotodev05@gmail.com>
2018-11-06 18:11:49 +0300

    Added Profile tab on profile component

--- src/components/users/Profile.vue
+++ src/components/users/Profile.vue
@@ -64,6 +64,53 @@
          <v-tab ripple href="#tab-2">
            Activity
          </v-tab>
+         <v-tabs-items v-model="selectedTab">
+           <v-tab-item id="tab-1">
+             <v-card flat>
+               <v-card-text>
+                 <v-list two-line class="pa-0">
+                   <v-list-tile href="#">
+                     <v-list-tile-action>
+                       <v-icon color="indigo">work</v-icon>
+                     </v-list-tile-action>
+                     <v-list-tile-content>
+                       <v-list-tile-title>System Administrator</v-list-tile-title>
+                       <v-list-tile-sub-title>Job Title</v-list-tile-sub-title>
+                     </v-list-tile-content>
+                     <v-list-tile-action>
+                     </v-list-tile-action>
+                   </v-list-tile>
+                   <v-divider inset></v-divider>
+                   <v-list-tile href="#">
+                     <v-list-tile-action>
+                       <v-icon color="indigo">phone</v-icon>
+                     </v-list-tile-action>
+                     <v-list-tile-content>
+                       <v-list-tile-title>+254703399398</v-list-tile-title>
+                       <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
+                     </v-list-tile-content>
+                     <v-list-tile-action>
+                       <v-icon>chat</v-icon>
+                     </v-list-tile-action>
+                   </v-list-tile>
+                   <v-divider inset></v-divider>
+                   <v-list-tile href="#">
+                     <v-list-tile-action>
+                       <v-icon color="indigo">mail</v-icon>
+                     </v-list-tile-action>
+                     <v-list-tile-content>
+                       <v-list-tile-title>kamaubrian05@gmail.com</v-list-tile-title>
+                       <v-list-tile-sub-title>Email</v-list-tile-sub-title>
+                     </v-list-tile-content>
+                     <v-list-tile-action>
+                       <v-icon>chat</v-icon>
+                     </v-list-tile-action>
+                   </v-list-tile>
+                 </v-list>
+               </v-card-text>
+             </v-card>
+           </v-tab-item>
+         </v-tabs-items>
        </v-tabs>
       </v-card-text>
     </v-card>
