32f64e583ff8d861192c8d7546c38e62df442e7f
mtotodev05 <mtotodev05@gmail.com>
2018-11-11 06:40:39 +0300

    Change layout of dialog box

--- src/components/users/admin/AddAdminstrator.vue
+++ src/components/users/admin/AddAdminstrator.vue
@@ -1,22 +1,13 @@
 <template>
-  <v-layout row justify-end>
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-      <v-btn slot="activator" class="green" flat dark fab small>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-btn slot="activator" class="green" dark flat fab small>
         <v-icon>add</v-icon>
       </v-btn>
-      <v-toolbar dark color="primarydark">
-        <v-btn icon dark @click.native="dialog=false">
-          <v-icon>close</v-icon>
-        </v-btn>
-        <v-toolbar-title>Add Administrator</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn dark flat @click.native="dialog=false">Save</v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-      <v-list three-line subheader>
-        <v-subheader>Add Administrator
-        <v-spacer></v-spacer>
+      <v-card>
+        <v-card-title>
+          <span class="headline">New Administrator</span>
+          <v-spacer></v-spacer>
           <v-avatar size="40px">
             <img src="/static/profile/man_profile.png" alt="admin"/>
           </v-avatar>
@@ -24,109 +15,52 @@
             <v-icon left dark>cloud_upload</v-icon>
             Upload Profile Image
           </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md6>
+                <v-text-field label="Enter First Name" required v-model="firstName"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md6>
+                <v-text-field label="Enter Last Name" v-model="lastName"></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Enter Email Address" required v-model="emailAddress"></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Enter PhoneNumber" required v-model="phoneNumber"></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Enter Password" type="password" required v-model="authenticationPassword"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+        </v-card-actions>
+        <v-progress-linear :indeterminate="true" v-if="loading"></v-progress-linear>
 
-        </v-subheader>
-
-        <v-list-tile avatar>
-          <v-list-tile-content>
-            <v-list-tile-sub-title>
-              <v-text-field
-                v-model="firstName"
-              append-icon="person"
-              name="password"
-              label="Enter First Name"
-              type="text"
-              required></v-text-field>
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile avatar>
-          <v-list-tile-content>
-            <v-list-tile-sub-title>
-              <v-text-field
-                v-model="lastName"
-                append-icon="person"
-                name="lastName"
-                label="Enter Last Name"
-                type="text"
-                required></v-text-field>
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-      <v-list three-line subheader>
-        <v-list-tile avatar>
-          <v-list-tile-content>
-            <v-list-tile-sub-title>
-              <v-text-field
-                v-model="emailAddress"
-                append-icon="mail"
-                name="emailAddress"
-                label="Enter Email Address"
-                type="email"
-                required></v-text-field>
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile avatar>
-          <v-list-tile-content>
-            <v-list-tile-sub-title>
-              <v-text-field
-                v-model="phoneNumber"
-                append-icon="phone"
-                name="phoneNumber"
-                label="Enter Phone Number"
-                type="number"
-                required></v-text-field>
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile avatar>
-          <v-list-tile-content>
-            <v-list-tile-sub-title>
-              <v-select
-                :items="roleTypes"
-              v-model="role"
-              label="Select Role Type"
-              auto
-              required
-              item-text="name"
-              item-value="id"></v-select>
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      </v-card>
     </v-dialog>
   </v-layout>
 </template>
 <script>
-  export default{
-    data(){
-      return{
-        dialog:false,
-        firstName:'',
-        lastName:'',
-        emailAddress:'',
-        phoneNumber:'',
-        role:'',
-        roleTypes:[
-          {
-            id:1,
-            name:'System Adminstrator'
-          },
-          {
-            id:2,
-            name:'Database Adminstrator'
-          },
-          {
-            id:3,
-            name:'System Maintainer'
-          },
-          {
-            id:4,
-            name:'Conductor'
-          }
-        ]
+  export default {
+    data: () => ({
+      dialog: false,
+      firstName:'',
+      lastName:'',
+      emailAddress:'',
+      phoneNumber:'',
+      authenticationPassword:''
+    }),
+    computed:{
+      loading(){
+        return this.$store.state.loading;
       }
     }
   }
