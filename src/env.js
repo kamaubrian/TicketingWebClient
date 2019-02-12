b9b3724e33058992c26aee714e41036270c1318a
mtotodev05 <mtotodev05@gmail.com>
2019-02-12 17:52:26 +0300

    Resolve env

--- src/env.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyAFuKG3rz2ClMZ5YJJQ0pf34ja0p41Z7Hk",
-  authDomain: "metro-c7c33.firebaseapp.com",
-  databaseURL: "https://metro-c7c33.firebaseio.com",
-  projectId: "metro-c7c33",
-  storageBucket: "metro-c7c33.appspot.com",
-  messagingSenderId: "789855294032"
-}
